3c41413b9a5c4b3f04cf1259d6f045f766bf905e
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2023-04-02 17:03:06 +0200

    remove unnecesasry comments

--- example2/math_test.go
+++ example2/math_test.go
@@ -1,16 +1,16 @@
 package math
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ExampleAdd() {
-	// Code block
 	{
 		fmt.Println(Add(1, 2))
 		// Output: 3
-	} // <--- This bracket will not present in output HTML
+	}
 
-	// Code block (second one). This block will not present in HTML at all
 	{
-		fmt.Println(Add(1, 2))
+		_ = Add(1, 2)
 	}
 }
